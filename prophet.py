67a52aa8da221fb65596accc56ae55baf4d71b3a
ilyamirin <ilya.mirin@gmail.com>
2020-05-23 23:49:51 +1000

    add normalization

--- prophet.py
+++ prophet.py
@@ -1,16 +1,39 @@
 from keras.models import Sequential
 from keras.layers import Dense
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data_file_path = '/Users/ilyamirin/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 data = pd.read_csv(data_file_path)
 
-train_data_x = data.iloc[0:, 4:120].values
-train_data_y = (data.iloc[0:, 5:121].values - train_data_x) / (train_data_x + 0.00000001)
+data_x = data.iloc[0:, 4:-1].values / data.iloc[0:, 4:].values.mean()
+data_y = data.iloc[0:, 5:].values / data.iloc[0:, 4:].values.mean()
 
 model = Sequential()
-model.add(Dense(1024, input_shape=(1,), activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(Dense(1024, input_shape=(1,), activation='relu', kernel_initializer='normal'))
+model.add(Dense(1, activation='relu', kernel_initializer='normal'))
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
-model.fit(train_data_x.flatten(), train_data_y.flatten(), epochs=150)
+history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64)
+
+model.predict(np.array([6053]))
+model.predict(np.array([326448]))
+model.predict(np.array([335882]))
+
+
+model.evaluate(data_x[:, -10:].flatten(), data_y[:, -10:].flatten())
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.show()
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.show()
