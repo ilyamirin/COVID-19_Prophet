b01b0f2143f3f2f22623b3a8579436cf04dee45d
ilyamirin <ilya.mirin@gmail.com>
2020-05-24 16:46:10 +1000

    prepare for deploy

--- prophet.py
+++ prophet.py
@@ -1,39 +1,43 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_file_path = '/Users/ilyamirin/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+data_file_path = '~/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 data = pd.read_csv(data_file_path)
 
-data_x = data.iloc[0:, 4:-1].values / data.iloc[0:, 4:].values.mean()
-data_y = data.iloc[0:, 5:].values / data.iloc[0:, 4:].values.mean()
+mean = data.iloc[0:, 4:].values.mean()
+data_x = data.iloc[0:, 4:-1].values / mean
+data_y = data.iloc[0:, 5:].values / mean
 
 model = Sequential()
-model.add(Dense(1024, input_shape=(1,), activation='relu', kernel_initializer='normal'))
-model.add(Dense(1, activation='relu', kernel_initializer='normal'))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.add(Dense(512, input_shape=(1,), activation='relu', kernel_initializer='normal'))
+model.add(Dense(1, kernel_initializer='normal'))
+model.compile(loss='mse', optimizer='adam')
 
-history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64)
+best_gen = (0, 1000000)
+for i in range(0, 10):
+    history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64, verbose=0)
+    mse = model.evaluate(data_x[:, -10:].flatten(), data_y[:, -10:].flatten())
+    if mse < best_gen[1]:
+        best_gen = (i, mse)
+        model.save("./model.h5")
+    print(i, mse, model.predict(np.array([969])), model.predict(np.array([326448])))
 
-model.predict(np.array([6053]))
-model.predict(np.array([326448]))
-model.predict(np.array([335882]))
+model1 = load_model("model.h5")
 
+print(model.predict(data.iloc[187:188, -1:].values[0])[0][0])
 
-model.evaluate(data_x[:, -10:].flatten(), data_y[:, -10:].flatten())
+model1.predict(np.array([969])) #981
+model1.predict(np.array([6053]))
+model1.predict(np.array([326448]))
+model1.predict(np.array([335882]))
 
 # summarize history for loss
 plt.plot(history.history['loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.show()
+#plt.show()
 
-# summarize history for accuracy
-plt.plot(history.history['accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.show()
