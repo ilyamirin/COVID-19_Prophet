bec4828e9b5d7432cb0db9552347826a5f403936
Ilya G. Mirin <ilya.mirin@gmail.com>
2020-07-14 16:55:44 +1000

    add prophesy

--- prophet.py
+++ prophet.py
@@ -3,8 +3,9 @@ from keras.layers import Dense
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-data_file_path = '~/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+data_file_path = '~/Projects/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 data = pd.read_csv(data_file_path)
 
 mean = data.iloc[0:, 4:].values.mean()
@@ -17,27 +18,33 @@ model.add(Dense(1, kernel_initializer='normal'))
 model.compile(loss='mse', optimizer='adam')
 
 best_gen = (0, 1000000)
-for i in range(0, 10):
-    history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64, verbose=0)
+for i in range(0, 5):
+    history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64, verbose=1)
     mse = model.evaluate(data_x[:, -10:].flatten(), data_y[:, -10:].flatten())
     if mse < best_gen[1]:
         best_gen = (i, mse)
         model.save("./model.h5")
-    print(i, mse, model.predict(np.array([969])), model.predict(np.array([326448])))
 
 model1 = load_model("model.h5")
 
-print(model.predict(data.iloc[187:188, -1:].values[0])[0][0])
+#retro
+real_date = data.iloc[:, -1:].axes[1].values[0]
+real = data.iloc[:, -1:].values
+pred_date = data.iloc[:, -2:-1].axes[1].values[0]
+pred = model1.predict(data.iloc[:, -2:-1].values).astype(int)
+delta = (real - pred) / (real+0.0000000001) * 100
 
-model1.predict(np.array([969])) #981
-model1.predict(np.array([6053]))
-model1.predict(np.array([326448]))
-model1.predict(np.array([335882]))
+result_data = np.concatenate((data.iloc[0:, 1:2], data.iloc[0:, 0:1], pred, real, delta.astype(int)), axis=1)
+result = pd.DataFrame(result_data, columns=['Страна', 'Провинция', 'Прогноз на %s' % real_date, 'Итог на %s' % real_date, 'Ошибка прогноза в %'])
 
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-#plt.show()
+result.to_csv('prediction.csv', sep=';')
+
+#new one
+pred = model1.predict(data.iloc[:, -1:].values).astype(int)
+(pred[187] - real[187]) / 2
+
+#model1.predict(np.array(1[969])) #981
+#model1.predict(np.array([6053]))
+#model1.predict(np.array([326448]))
+#model1.predict(np.array([335882]))
 
