6bf8c601708d382e8ed35afd4658f129f7f4a8b7
Ilya G. Mirin <ilya.mirin@gmail.com>
2020-10-02 10:14:29 +1000

    fix for pak

--- prophet.py
+++ prophet.py
@@ -8,18 +8,18 @@ import sys
 data_file_path = '~/Projects/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 data = pd.read_csv(data_file_path)
 
-mean = data.iloc[0:, 4:].values.mean()
-data_x = data.iloc[0:, 4:-1].values / mean
-data_y = data.iloc[0:, 5:].values / mean
+mean = data.iloc[0:, 4:100].values.mean()
+data_x = data.iloc[0:, 4:99].values / mean
+data_y = data.iloc[0:, 5:100].values / mean
 
 model = Sequential()
-model.add(Dense(512, input_shape=(1,), activation='relu', kernel_initializer='normal'))
+model.add(Dense(1024, input_shape=(1,), activation='relu', kernel_initializer='normal'))
 model.add(Dense(1, kernel_initializer='normal'))
 model.compile(loss='mse', optimizer='adam')
 
 best_gen = (0, 1000000)
 for i in range(0, 5):
-    history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64, verbose=1)
+    history = model.fit(data_x[:, :-10].flatten(), data_y[:, :-10].flatten(), epochs=64, verbose=1, batch_size=1024)
     mse = model.evaluate(data_x[:, -10:].flatten(), data_y[:, -10:].flatten())
     if mse < best_gen[1]:
         best_gen = (i, mse)
@@ -43,8 +43,16 @@ result.to_csv('prediction.csv', sep=';')
 pred = model1.predict(data.iloc[:, -1:].values).astype(int)
 (pred[187] - real[187]) / 2
 
-#model1.predict(np.array(1[969])) #981
-#model1.predict(np.array([6053]))
-#model1.predict(np.array([326448]))
-#model1.predict(np.array([335882]))
+model1.predict(np.array([969])) #981
+model1.predict(np.array([6053]))
+model1.predict(np.array([326448]))
+model1.predict(np.array([335882]))
+model1.predict(np.array([922853])) #927745
+model1.predict(np.array([1030690]))
+
+s = 200
+for i in range(0, 100):
+    n = model1.predict(np.array([s]))[0][0]
+    print(n)
+    s = n
 
